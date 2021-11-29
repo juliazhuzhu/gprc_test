9f5e712d5b22e11a01b464906319c3f7deb9cd30
xiaoye <3144604@qq.com>
2021-11-29 09:23:40 +0800

    add a func to extract the key field

--- c22/gin_start/ch07/main.go
+++ c22/gin_start/ch07/main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
-	"github.com/go-playground/locales/en"
-	"github.com/go-playground/locales/zh"
+	en "github.com/go-playground/locales/en"
+	zh "github.com/go-playground/locales/zh"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
 	"net/http"
+	"reflect"
+	"strings"
 
-	en_translations "github.com/go-playground/validator/v10/translations/en"
-	cn_translations "github.com/go-playground/validator/v10/translations/zh"
+	entranslations "github.com/go-playground/validator/v10/translations/en"
+	cntranslations "github.com/go-playground/validator/v10/translations/zh"
 )
 
 type LoginForm struct {
@@ -29,25 +31,46 @@ type SignUpForm struct {
 
 var trans ut.Translator
 
+func removeTopStruct(fields map[string]string) map[string]string {
+	rsp := make(map[string]string,0)
+
+	for field , value := range fields {
+		fmt.Println(field, value)
+		rsp[field[strings.Index(field, ".")+1:]] = value
+	}
+
+	return rsp
+
+}
+
 func InitTrans(locale string) (err error) {
 	//to custom validator property
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		cn := zh.New()
+
+		v.RegisterTagNameFunc(func (fld reflect.StructField)string {
+			name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+			if name == "-" {
+				return ""
+			}
+
+			return name
+		})
+		zh := zh.New()
 		en := en.New()
-		//default en, suport cn, and en
-		uni := ut.New(en, cn, en)
-		trans, ok = uni.GetTranslator("zh")
+		//default en, support cn, and en
+		uni := ut.New(en, zh, en)
+		trans, ok = uni.GetTranslator(locale)
 		if !ok {
 			return fmt.Errorf("uni.GetTranslator(%s", locale)
 
 		}
 		switch locale {
 		case "en":
-			en_translations.RegisterDefaultTranslations(v, trans)
-		case "cn":
-			cn_translations.RegisterDefaultTranslations(v, trans)
+			entranslations.RegisterDefaultTranslations(v, trans)
+		case "zh":
+			cntranslations.RegisterDefaultTranslations(v, trans)
 		default:
-			en_translations.RegisterDefaultTranslations(v, trans)
+			entranslations.RegisterDefaultTranslations(v, trans)
 		}
 	}
 
@@ -56,7 +79,8 @@ func InitTrans(locale string) (err error) {
 }
 
 func main() {
-	if err := InitTrans("cn"); err != nil {
+	
+	if err := InitTrans("zh"); err != nil {
 		fmt.Println("init trans error")
 		return
 	}
@@ -73,7 +97,7 @@ func main() {
 			}
 
 			c.JSON(http.StatusBadRequest, gin.H{
-				"error": errs.Translate(trans),
+				"error": removeTopStruct(errs.Translate(trans)),
 			})
 			return
 		}
